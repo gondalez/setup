4cf0c7c40d26db38f4a986fb1c518ef4fc98acfb
Tristan Harmer <gondalez@gmail.com>
2015-04-04 16:36:04 +0800

    Better prelude, personal emacs config

---
+++ roles/emacs/files/personal.el
@@ -0,0 +1,12 @@
+;;; package --- Summary
+;;; Commentary:
+
+;;; Code:
+
+(setq mac-option-modifier 'super)
+(setq mac-command-modifier 'meta)
+
+;; testing 123
+
+(provide 'personal)
+;;; personal.el ends here
