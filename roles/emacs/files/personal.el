e8e0699278128a0720d545fb4f209c138ccc0ec8
Tristan Harmer <gondalez@gmail.com>
2015-04-06 11:09:33 +0800

    More emacs conf

--- roles/emacs/files/personal.el
+++ roles/emacs/files/personal.el
@@ -3,13 +3,15 @@
 
 ;;; Code:
 
+;;; keyboard
 (setq mac-option-modifier 'super)
 (setq mac-command-modifier 'meta)
 
+;;; appearance
 (set-face-attribute 'default nil :font "Source Code Pro Light-14")
 (set-frame-font "Source Code Pro Light" nil t)
-
 (desktop-save-mode 1)
+(scroll-bar-mode 0)
 
 (provide 'personal)
 ;;; personal.el ends here
