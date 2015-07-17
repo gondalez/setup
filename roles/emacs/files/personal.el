4e0b1c6163356a8fdd49f17ce578d9ad80aee323
gondalez <gondalez@gmail.com>
2015-07-17 10:38:53 +0800

    update

--- roles/emacs/files/personal.el
+++ roles/emacs/files/personal.el
@@ -3,6 +3,10 @@
 
 ;;; Code:
 
+;;; mode <-> extension associations
+(add-to-list 'auto-mode-alist '("\\.cjsx\\'" . coffee-mode))
+
+
 ;;; keyboard
 (setq mac-option-modifier 'super)
 (setq mac-command-modifier 'meta)
@@ -12,6 +16,11 @@
 (set-frame-font "Source Code Pro Light" nil t)
 (desktop-save-mode 1)
 (scroll-bar-mode 0)
+
+;;; hightlight bad stuff
+(setq whitespace-style '(tabs empty trailing))
+;;; line-tail hightlights the end of long lines
+;;; (setq whitespace-line-column 80)
 
 ;;; multiple cursors
 (require 'multiple-cursors)
@@ -25,9 +34,11 @@
 (setq web-mode-markup-indent-offset 2)
 (setq web-mode-css-indent-offset 2)
 (setq web-mode-code-indent-offset 2)
+(setq json-reformat:indent-width 2)
 
 ;;; magit
 (setq magit-status-buffer-switch-function 'switch-to-buffer) ;;; full size stat
+(setq magit-last-seen-setup-instructions "1.4.0")
 
 ;;; time tracking
 (global-wakatime-mode)
@@ -37,6 +48,10 @@
 
 ;; disable company mode (COMplete ANYwhere) auto complete
 (setq global-company-mode 0)
+
+;;; start dat server
+(server-start)
+(setq projectile-enable-idle-timer 10)
 
 (provide 'personal)
 ;;; personal.el ends here
